f6595e2e244ffc54ef731db14728b0be06748fae
Malachi Mccormick <malachimccormick@Malachis-MBP.localdomain>
2019-02-18 12:00:50 -0500

    changed tag

--- src/App.js
+++ src/App.js
@@ -5,8 +5,10 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <h1>This is the Dev branch</h1>
+      <div>
+      <h1>This is the conflict2 branch</h1>
       <h2>This is an h2</h2>
+      </div>
     );
   }
 }
